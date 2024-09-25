fd97e1aa10e9b0bb6544f27002a6ac5a7c3312c7
Gerald Allawan <20181775@s.ubaguio.edu>
2024-09-25 15:43:10 +0800

    Add files via upload
    SRC for react-color-picker

---
+++ src (color-picker)/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer(){
+    return(
+        <>
+        <footer>
+            <hr />
+            <p>
+                &copy;
+                {new Date().getFullYear}
+                My Color Picker Website ||
+                Written by: Gerald Prince Allawan
+            </p>
+        </footer>
+        </>
+    )
+}
